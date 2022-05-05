952c7976f3dec04c209e29f5f9cc9d91ea1a7943
davherari-amu <david.hernandez-aristizabal@univ-amu.fr>
2022-05-05 12:11:33 +0200

    Make doi2bib a callable python function.

--- doi2bib.py
+++ doi2bib.py
@@ -1,29 +1,36 @@
 """Code copied from:
 https://scipython.com/blog/doi-to-bibtex/
 """
-
+# Libraries {{{
 import sys
 import urllib.request
 from urllib.error import HTTPError
+# }}}
 
+# Parameters {{{
 BASE_URL = 'http://dx.doi.org/'
+# }}}
 
-try:
-    doi = sys.argv[1]
-except IndexError:
-    print('Usage:\n{} <doi>'.format(sys.argv[0]))
-    sys.exit(1)
+# Functions {{{
+def doi2bib(doi):
+    url = BASE_URL + doi
+    req = urllib.request.Request(url)
+    req.add_header('Accept', 'application/x-bibtex')
+    try:
+        with urllib.request.urlopen(req) as f:
+            bibtex = f.read().decode()
+    except HTTPError as e:
+        if e.code == 404:
+            print('DOI not found.')
+        else:
+            print('Service unavailable.')
+        sys.exit(1)
+    return bibtex
+# }}}
 
-url = BASE_URL + doi
-req = urllib.request.Request(url)
-req.add_header('Accept', 'application/x-bibtex')
-try:
-    with urllib.request.urlopen(req) as f:
-        bibtex = f.read().decode()
-    print(bibtex)
-except HTTPError as e:
-    if e.code == 404:
-        print('DOI not found.')
-    else:
-        print('Service unavailable.')
-    sys.exit(1)
+# Test {{{
+if __name__ == '__main__':
+    print('Example with 10.1177/1470593113512323')
+    doi = '10.1177/1470593113512323'
+    print(doi2bib(doi))
+# }}}
