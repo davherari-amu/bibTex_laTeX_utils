045bbf54f8bf4bc73e0e56222d9db22a3879dcfd
davherari-amu <david.hernandez-aristizabal@univ-amu.fr>
2022-05-09 09:38:12 +0200

    Allow module version.

--- unifyBib.py
+++ unifyBib.py
@@ -6,39 +6,6 @@ from pybtex.database import BibliographyData as bbd
 from pybtex.database import Entry
 # }}}
 
-# Input and output files {{{
-try:
-    opts, args = getopt.getopt(sys.argv[1:], 'i:o:l:')
-    if len(args)>0:
-        raise getopt.GetoptError('Too many arguments!')
-except getopt.GetoptError:
-    print('Wrong call! The excecution can have the following arguments:')
-    print('To indicate the input file name: -i "fileName.bib"')
-    print('To indicate the output file name: -o "fileName.bib" (optional)')
-    print('To indicate the length of the author last-name letters: -l length (optional)')
-    raise
-# Initialisation
-inFile  = None
-outFile = None
-lenAutName = 3
-for opt, arg in opts:
-    # File name
-    if opt in ['-i']:
-        inFile = arg
-    if opt in ['-o']:
-        outFile = arg
-    if opt in ['-l']:
-        lenAutName = eval(arg)
-if inFile == None:
-    raise("Wrong call! The input file is not present. Use -i 'fileName.json'")
-if outFile == None:
-    outFile = inFile
-if not inFile[-4:] == '.bib':
-    raise("Error: the input file must be a .bib file")
-if not outFile[-4:] == '.bib':
-    raise("Error: the output file must be a .bib file")
-# }}}
-
 # Functions {{{
 # Add DOI as a note {{{
 def AddDoiAsNote(bibData, message = 'DOI:'):
@@ -87,19 +54,57 @@ def UnifiedEntryName(bibData, maxNumAutLets = 3):
     newBib = bbd()
     for newKey in keys:
         oldKey = keys[newKey]
-        type_ = bib.entries[oldKey].type
-        fields = bib.entries[oldKey].fields
-        persons = bib.entries[oldKey].persons
+        type_   = bibData.entries[oldKey].type
+        fields  = bibData.entries[oldKey].fields
+        persons = bibData.entries[oldKey].persons
         entry = Entry(type_, fields, persons)
         newBib.entries[newKey] = entry
     # }}}
     return newBib
 # }}}
+
+# Uni_Doi_and_name
+def Uni_Doi_and_name(inFile, outFile, lenAutName = 3):
+    bib = parse_file(inFile)
+    bib = AddDoiAsNote(bib)
+    bib = UnifiedEntryName(bib, maxNumAutLets = lenAutName)
+    with open(outFile, 'w') as File:
+        File.write(bib.to_string("bibtex"))
 # }}}
 
-
-bib = parse_file(inFile)
-bib = AddDoiAsNote(bib)
-bib = UnifiedEntryName(bib, maxNumAutLets = lenAutName)
-with open(outFile, 'w') as File:
-    File.write(bib.to_string("bibtex"))
+# As executable script {{{
+if __name__ == '__main__':
+    # Input and output files {{{
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'i:o:l:')
+        if len(args)>0:
+            raise getopt.GetoptError('Too many arguments!')
+    except getopt.GetoptError:
+        print('Wrong call! The excecution can have the following arguments:')
+        print('To indicate the input file name: -i "fileName.bib"')
+        print('To indicate the output file name: -o "fileName.bib" (optional)')
+        print('To indicate the length of the author last-name letters: -l length (optional)')
+        raise
+    # Initialisation
+    inFile  = None
+    outFile = None
+    lenAutName = 3
+    for opt, arg in opts:
+        # File name
+        if opt in ['-i']:
+            inFile = arg
+        if opt in ['-o']:
+            outFile = arg
+        if opt in ['-l']:
+            lenAutName = eval(arg)
+    if inFile == None:
+        raise("Wrong call! The input file is not present. Use -i 'fileName.json'")
+    if outFile == None:
+        outFile = inFile
+    if not inFile[-4:] == '.bib':
+        raise("Error: the input file must be a .bib file")
+    if not outFile[-4:] == '.bib':
+        raise("Error: the output file must be a .bib file")
+    # }}}
+    Uni_Doi_and_name(inFile, outFile, lenAutName)
+# }}}
